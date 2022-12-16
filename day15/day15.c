d6d7b6917183707f9d96195f5bcd5a7b13cc636e
Paul Caporn <paul.caporn@bbc.co.uk>
2022-12-16 08:13:24 +0000

    Part 2 with the wrong answer

--- day15/day15.c
+++ day15/day15.c
@@ -7,6 +7,14 @@ struct range {
     int end;
 };
 
+struct entry {
+    int sx;
+    int sy;
+    int bx;
+    int by;
+    int dist;
+};
+
 int compare(const void* l, const void* r){
     struct range* range1 = *(struct range **)l;
     struct range* range2 = *(struct range **)r;
@@ -37,26 +45,40 @@ void merge_ranges(struct range** ranges, int n, struct range** merged_ranges, in
 }
 
 int main(){
-    struct file_contents fc = read_lines("input");
-    int sx,sy,bx,by;
+    int max_pos = 4000000;
     int target_row = 2000000;
+    char* filename = "input";
+
+    // int max_pos = 20;
+    // int target_row = 10;
+    // char* filename = "input_test";
+
+    struct file_contents fc = read_lines(filename);
+    int entry_count = fc.line_count;
+    struct entry entries[entry_count];
+    for (int i=0;i <entry_count;i++){
+        struct entry e;
+        sscanf(fc.lines[i], "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &e.sx, &e.sy, &e.bx, &e.by);
+        e.dist = abs(e.sx-e.bx) + abs(e.sy-e.by);
+        entries[i] = e;
+    }
+
+    // Part 1    
     int n = 0;
     struct range** ranges = malloc(fc.line_count * sizeof(struct range));
-    for (int i=0;i < fc.line_count;i++){
-        sscanf(fc.lines[i], "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sx, &sy, &bx, &by);
-        int dist = abs(sx-bx) + abs(sy-by);
-        if (abs(sy - target_row) < dist) {
-            int len = dist - abs(sy - target_row);
-            int xmin = sx - len;
-            int xmax = sx + len;
+    for (int i=i;i<entry_count;i++){
+        struct entry e = entries[i];
+        if (abs(e.sy - target_row) < e.dist) {
+            int len = e.dist - abs(e.sy - target_row);
+            int xmin = e.sx - len;
+            int xmax = e.sx + len;
             struct range* r = malloc(sizeof(struct range));
             r->start = xmin;
             r->end = xmax;
             ranges[n++] = r;            
         }
     }
-    qsort(ranges, n, sizeof(struct range), compare);
-    
+    qsort(ranges, n, sizeof(struct range), compare);    
     struct range** merged_ranges = malloc(n * sizeof(struct range **));
     int merged_size;
     merge_ranges(ranges, n, merged_ranges, &merged_size);
@@ -72,5 +94,36 @@ int main(){
     total = total - 1;
     printf("Part 1 - %i\n", total);
 
-    
+    // Part 2
+    // walk each entry
+    // check if the bounary is outside all the other entries
+    for (int i=0;i<entry_count;i++){
+        // check all the positions on the boundary of the entry
+        for (int j=0; j<entries[i].dist; j++){
+            int positions[4][2]={ 
+                { entries[i].sx - (entries[i].dist + 1 - j), entries[i].sy - j },
+                { entries[i].sx + (entries[i].dist + 1 - j), entries[i].sy - j },
+                { entries[i].sx - (entries[i].dist + 1 - j), entries[i].sy + j },
+                { entries[i].sx + (entries[i].dist + 1 - j), entries[i].sy + j },
+            };
+            for (int pos = 0; pos < 4; pos++){
+                if ( positions[pos][0] < 0 || positions[pos][0] > max_pos) continue;
+                if ( positions[pos][1] < 0 || positions[pos][1] > max_pos) continue;
+                int found = 1;
+                for (int n=0;n<entry_count && found;n++){
+                    if (i == n) continue;            
+                    // check if entry contains pos
+                    // it does if the manhattan distance is less then the distance to the pos.
+                    int pos_dist = abs(entries[n].sx - positions[pos][0]) + abs(entries[n].sy - positions[pos][1]);
+                    if (pos_dist <= entries[n].dist){
+                        found = 0;
+                    }
+                }
+                if (found){
+                    u_int64_t tuning_freq = (u_int64_t)(positions[pos][0] * 4000000) + positions[pos][1];
+                    printf("Part 2 - (%i,%i) - %llu\n", positions[pos][0], positions[pos][1], tuning_freq);
+                }
+            }
+        }
+    }
 }
